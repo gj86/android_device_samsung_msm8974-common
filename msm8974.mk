133ab2cbbca7adad7d23bfd502e7898db44c65fa
Bruno Martins <bgcngm@gmail.com>
2018-01-26 11:27:35 +0000

    msm8974-common: Binderize them all v2
    * Switch to binderized HAL services as possible and update
       HIDL manifest accordingly.
    
    Change-Id: Id50291488d655187aa013c51bdd6890dca010564
    Signed-off-by: Bruno Martins <bgcngm@gmail.com>
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- msm8974.mk
+++ msm8974.mk
@@ -48,6 +48,7 @@ PRODUCT_COPY_FILES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8974 \
@@ -71,6 +72,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
