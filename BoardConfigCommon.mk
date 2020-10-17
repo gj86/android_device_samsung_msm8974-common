463e2a893e49d8aa392ad5ecc00e47efb3a9cee2
Kyle Harrison <khwebmail@gmail.com>
2020-10-17 13:56:19 +0100

    msm8974-common: Update dexpreopt config
    - WITH_DEXPROPT is now enabled by default so current dexpreopt config
    has no effect
    - Enable DONT_DEXPREOPT_PREBUILTS to save some space on /system
    - Remove debug info
    
    Change-Id: I91372c2c7b476267a6e395fbbacb4c39f9bc1a5c
    Signed-off-by: Gobinda Joy <gobinda.joy@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -52,8 +52,10 @@ WITH_LINEAGE_CHARGER := false
 # Dexpreopt
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= false
-    WITH_DEXPREOPT := true
+    WITH_DEXPREOPT_DEBUG_INFO := false
+    USE_DEX2OAT_DEBUG := false
+    DONT_DEXPREOPT_PREBUILTS := true
+    WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
   endif
 endif
 
